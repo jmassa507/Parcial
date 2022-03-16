37a8007cac2786c71ff67f0df68e1ababa960ab8
jmassa507 <99853128+jmassa507@users.noreply.github.com>
2022-03-16 02:10:11 -0500

    Add files via upload

---
+++ Examen parcial.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <stdlib.h>
+
+using namespace std;
+
+/*Práctica (grupal):
+1) Desarrolle el siguiente problema en base a los conceptos dados en
+clase:
+La universidad XYZ necesita desarrollar un sistema online en el cualsus
+estudiantes puedan revisar su estado de los tickets para quejas y sugerencias.
+Para esto, se necesita desarrollar la lógica inicial de la siguiente manera:
+a. Los usuarios(Estudiantes) deberán ingresarsusrespectivas
+credenciales(usuario y contraseña) qué, inicialmente se las otorga el
+propio sistema de la universidad. (Se debe tomar en consideración
+que la institución universitaria, solo otorga este sistema a los
+usuarios que mantengan una matrícula activa).
+b. Una vez el cliente haya ingresado por primera vez al sistema, el
+mismo debe mostrar la fecha y hora de ingreso. Y, a su vez, le debe
+pedir que actualice (cambie) la contraseña.
+c. Luego de haber cambiado la contraseña, elsistema debe mostrar al
+cliente un menú con el siguiente formato:
+1) Subir un ticket: Deberá permitir que el usuario ingrese la
+información para el envío de su ticket siguiendo el siguiente patrón:
+nombre, apellido, identificación, correo y carrera que estudia;
+seguido por el ticket que desee enviar.
+Una vez enviado el ticket, el mismo deberá generar un código o
+identificador del ticket
+2) Ver tickets: El usuario podrá el código o identificador de los tickets
+que haya creado en el sistema
+3) Salir: Termina el proceso y debe mostrar la fecha y hora en la que el
+cliente cerró la sesión*/
+
+/*Integrantes: Juan Massa, David Lopez, Israel Castillo, Luis Zhang, Saul Rojas*/
+
+/*User y password inicial: 123/123*/
+
+using namespace std;
+int main() 
+{
+	int user, pass;
+	
+	cout<<"Bienvenido al sistema online de la Universidad XYZ, por favor ingrese su usuario: "<<endl;
+	cin>>user;
+	cout<<"Ingrese su password: "<<endl;
+	cin>>pass;
+	cout<<"Por favor, cambie su password: ";
+
+}
+	
+
